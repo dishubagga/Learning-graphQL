b89187ddd29e082a32ad48fcfa1542fa654a8dda
Dishu Bagga <dishubagga9111@gmail.com>
2020-06-02 18:17:22 +0200

    Adding schema for accessing id and name

--- server.js
+++ server.js
@@ -3,21 +3,71 @@ const expressGraphQL = require('express-graphql')
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql') //importing schema and object type for creating different types
 const app = express()
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: { 
-                type: GraphQLString,
-                resolve: ()=> 'Hello World'
-            }
-        })
+const authors = [
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J.R.R tolkein' },
+    { id: 3, name: 'Brent Weeks'}
+
+]
+
+const books = [
+    { id: 1, name: 'Harry Potter and the chamber of secrets', authorId: 1},
+    { id: 2, name: 'Harry Potter and the prisoner of Azkaban', authorId: 1},
+    { id: 3, name: 'Harry Potter and the Globlet of fire', authorId: 1},
+    { id: 4, name: 'Fellowship of the ring', authorId: 2},
+    { id: 5, name: 'The two Towers', authorId: 2},
+    { id: 6, name: 'The return of king', authorId: 2},
+    { id: 7, name: 'The way of shadows', authorId: 3},
+    { id: 3, name: 'Beyond the sahdows', authorId: 3},
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This Respresents the book written by author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString)},
+        authorId: { type: GraphQLNonNull(GraphQLInt) }
     })
 })
+
+
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () =>({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List Of All Books',
+            resolve: () => books
+        }
+    })
+})
+
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+// const schema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//         name: 'HelloWorld',
+//         fields: () => ({
+//             message: { 
+//                 type: GraphQLString,
+//                 resolve: ()=> 'Hello World'
+//             }
+//         })
+//     })
+// })
 
 
 app.use('/graphql', expressGraphQL({
