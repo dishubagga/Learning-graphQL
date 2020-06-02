2e015d94a4e2fa3f03c56b4a086ce0357f85281a
Dishu Bagga <dishubagga9111@gmail.com>
2020-06-02 17:42:01 +0200

    Setting up the basics

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const app = express()
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
+app.listen(5000., ()=> console.log('Server Running'))
